62e5a24ad37cd23d5013a0e554f37c4b47335ad0
two9ne <94082813+two9ne@users.noreply.github.com>
2025-10-07 19:46:50 +0200

    Update MainController.py

--- backend/app/controllers/MainController.py
+++ backend/app/controllers/MainController.py
@@ -10,3 +10,60 @@ class MainController(Controller):
 
         response = {"result": result}
         return JSONResponse(response)
+
+    def anyrun(request: Request):
+
+        plugin = plugins["AnyRun"]
+
+        response = {"result": result}
+        return JSONResponse(response)
+
+    def malshare(request: Request):
+
+        plugin = plugins["MalShare"]
+
+        response = {"result": result}
+        return JSONResponse(response)
+
+    def malwarebazaar(request: Request):
+
+        plugin = plugins["MalwareBazaar"]
+
+        response = {"result": result}
+        return JSONResponse(response)
+
+    def kaspersky(request: Request):
+
+        plugin = plugins["Kaspersky"]
+
+        response = {"result": result}
+        return JSONResponse(response)
+
+    def hybridanalysis(request: Request):
+
+        plugin = plugins["HybridAnalysis"]
+
+        response = {"result": result}
+        return JSONResponse(response)
+
+    def triage(request: Request):
+
+        plugin = plugins["Triage"]
+
+        response = {"result": result}
+        return JSONResponse(response)
+
+    def metadefender(request: Request):
+
+        plugin = plugins["MetaDefender"]
+
+        response = {"result": result}
+        return JSONResponse(response)
+
+    def threatfox(request: Request):
+
+        plugin = plugins["ThreatFox"]
+
+        response = {"result": result}
+        return JSONResponse(response)
+
